e8e8e4db7ab21c0c6c8783e181e2c500d200c581
jo83work-cloud <jo83work@gmail.com>
2025-12-31 16:34:23 +0100

    Update setup_jumphost.sh

--- setup_jumphost.sh
+++ setup_jumphost.sh
@@ -3,3 +3,15 @@
 ## Create RG 
 az group create --name learnAzureRG --location westeurope
 
+
+## Spin Jumphost VM
+az vm create \
+  --resource-group learnAzureRG \
+  --name jumphostVM \
+  --image RedHat:RHEL:9-lvm:latest \
+  --size Standard_D2s_v6 \
+  --admin-username azureuser \
+  --generate-ssh-keys
+
+
+
